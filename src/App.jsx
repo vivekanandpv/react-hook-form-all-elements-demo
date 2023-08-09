e2635aedf41269a20b1efb3a1ab290d45057589c
Vivekanand P V <vivekanandpv@outlook.com>
2023-08-09 16:42:32 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import SampleForm from './SampleForm';
+
+const App = (props) => {
+  return (
+    <>
+      <div className='p-5'>
+        <h2>React Hook Forms Demo</h2>
+        <hr />
+        <SampleForm />
+      </div>
+    </>
+  );
+};
+
+export default App;
